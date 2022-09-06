01d8f8bb209ceb3782d4df41514446166eadb0fd
전준영 <wnsdud9476@DESKTOP-2VICEL1.localdomain>
2022-09-06 17:03:04 +0900

    error추가

--- controllers/products_detail_controller.js
+++ controllers/products_detail_controller.js
@@ -7,6 +7,9 @@ const productsDetailController = async (req, res) => {
       res.status(400 || 500).json({ ERROR: "Not existing product" });
       return;
     }
+    if (!id) {
+      res.status(400 || 500).json({ ERROR: "CHECK Id Data" });
+    }
     const productDetail = await productsDetailService.productDetail(id);
 
     res.status(200).json({ productDetail });
